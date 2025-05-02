eb4cc8178a776cc686d2aaf5887bd643ff39e0af
Muhammad Al Farizi <ziziyusrizal@gmail.com>
2025-05-02 13:11:32 +0200

    chore: connected lobby and prologue forest 01 for development

--- game_controller.gd
+++ game_controller.gd
@@ -9,7 +9,7 @@ var current_2d_scene: Room
 
 func _ready() -> void:
 	Global.game_controller = self
-	current_2d_scene = %Prologue_forest_06
+	current_2d_scene = %Prologue_forest_01
 	Global.dialogue = %DialogueHud
 	SceneManager.entry_door_changed.connect(_on_entry_door_changed)
 	loading_screen.animation_player.animation_finished.connect(func(anim_name): _on_animation_finished(anim_name))
